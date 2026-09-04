3423d47a823acb238fb8c9c03c2b981f233c0cc4
cwcarrillov <cwcarrillov@outlook.es>
2026-09-03 22:17:31 -0500

    Add module selection and display messages

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ st.sidebar.title("Parámetros")
 st.write("Elaborado por: Carlos Carrillo")
 
 modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de listas")
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de arreglos")
+
+elif modulos == "Funciones":
+  st.write("Te encuentras en el módulo de Funciones")
+  
+else:
+  st.write("Te encuentras en el módulo de POO")
+  
