61040afd3ecf326ec8b844e17e82f69632d5d123
cwcarrillov <cwcarrillov@outlook.es>
2026-09-03 22:19:36 -0500

    Add number input for initial and final range values

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Fu
 if modulos == "Listas":
   st.write("Te encuentras en el módulo de listas")
 
+  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
+  valor_final = st.number_input("]Ingresa tu valor final del rango")
+
 elif modulos == "Arreglos":
   st.write("Te encuentras en el módulo de arreglos")
 
