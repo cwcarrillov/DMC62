2f6af917c4b8634f22fa24c793969936b8e6af2a
cwcarrillov <cwcarrillov@outlook.es>
2026-09-03 22:23:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Fu
 if modulos == "Listas":
   st.write("Te encuentras en el módulo de listas")
 
-  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
-  valor_final = st.number_input("]Ingresa tu valor final del rango")
+  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
+  valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
+
+  lista = list(range(valor_inicial, valor_final))
+
+  st.write(lista)
 
 elif modulos == "Arreglos":
   st.write("Te encuentras en el módulo de arreglos")
