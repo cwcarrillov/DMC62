732342818331bf9b1a26b8a7eaf8a66d4c334dd7
cwcarrillov <cwcarrillov@outlook.es>
2026-09-08 22:19:39 -0500

    Increase logo width from 100 to 300

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Carlos Carrillo")
 
-st.image("Python_logo.png",width =100)
+st.image("Python_logo.png",width =300)
 
 
 modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
